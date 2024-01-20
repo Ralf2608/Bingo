46bebec344d82b3fac9ead82b4446f06f58a138b
Rafael <ralfsantanasantos@gmail.com>
2024-01-20 15:00:13 -0300

    Remoção do número central

--- src/components/Columns.js
+++ src/components/Columns.js
@@ -1,10 +1,14 @@
 import { View, Text, StyleSheet } from "react-native"
 
 export default ({i,numbers}) =>{
+    
     return(
         <View style={style.row}>
             {<Text>{i}</Text>}
-            {numbers.map((number)=>{
+            {numbers.map((number, index)=>{
+                if(i === 'N' && index === 2){
+                    return <Text></Text>
+                }
                 return <Text>{number}</Text>
             })}
         </View>
